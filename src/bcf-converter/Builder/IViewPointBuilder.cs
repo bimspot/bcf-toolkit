3fc99a5d6238fe8eed81353f50959687ce4dc01e
Balint Bende <balint@bimspot.io>
2023-10-10 13:57:15 +0200

    [BMSPT-24] added licence and readme, nuget pack, docs

--- src/bcf-converter/Builder/IViewPointBuilder.cs
+++ src/bcf-converter/Builder/IViewPointBuilder.cs
@@ -14,26 +14,17 @@ public interface IViewPointBuilder<
   out TClippingPlaneBuilder,
   out TBitmapBuilder> :
   IBuilder<IVisualizationInfo> {
+  /// <summary>
+  ///   Returns the builder object extended with `Guid`.
+  /// </summary>
+  /// <param name="guid">The version id.</param>
+  /// <returns>Returns the builder object.</returns>
   TBuilder AddGuid(string guid);
-
   TBuilder AddSelection(Action<TComponentBuilder> builder);
   TBuilder AddVisibility(Action<TVisibilityBuilder> builder);
   TBuilder AddColoring(Action<TColorBuilder> builder);
-
   TBuilder AddOrthogonalCamera(Action<TOrthogonalCameraBuilder> builder);
-
-  // TODO 3.0
-  // TBuilder AddOrthogonalCamera(
-  //   Action<ICameraBuilder> cameraBuilder,
-  //   double viewToWorldScale, 
-  //   double aspectRatio);
   TBuilder AddPerspectiveCamera(Action<TPerspectiveCameraBuilder> builder);
-
-  // TODO 3.0
-  // TBuilder AddPerspectiveCamera(
-  //   Action<ICameraBuilder> cameraBuilder,
-  //   double fieldOfView,
-  //   doubel aspectRatio);
   TBuilder AddLine(Action<TLineBuilder> builder);
   TBuilder AddClippingPlane(Action<TClippingPlaneBuilder> builder);
   TBuilder AddBitmap(Action<TBitmapBuilder> builder);
