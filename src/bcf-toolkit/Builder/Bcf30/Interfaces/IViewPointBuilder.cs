1d65441f861e7d76f826f82fb7b2b5d8ee14d011
Balint Bende <balint@bimspot.io>
2024-06-04 11:22:35 +0200

    [BMSPT-289] fixed location and direction instance creation in bitmap, clipping plane and line builders

---
+++ src/bcf-toolkit/Builder/Bcf30/Interfaces/IViewPointBuilder.cs
@@ -0,0 +1,9 @@
+namespace BcfToolkit.Builder.Bcf30.Interfaces;
+
+public class IViewPointBuilder<out TBuilder> {
+  TBuilder SetViewPoint(string viewpoint);
+  TBuilder SetSnapshot(string viewpoint);
+  TBuilder SetSnapshotData(string viewpoint);
+  TBuilder SetViewPoint(string viewpoint);
+  TBuilder SetViewPoint(string viewpoint);
+}
