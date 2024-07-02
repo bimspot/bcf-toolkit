31d5e3bb62f1ae2cf7d1506a9a40766ae72668f7
Balint Bende <balint@bimspot.io>
2024-07-02 09:47:10 +0200

    [NOISSUE] disabled range requirement for `FieldOfView` in BCF 2.1

--- src/bcf-toolkit/Model/Bcf21/VisInfo.cs
+++ src/bcf-toolkit/Model/Bcf21/VisInfo.cs
@@ -114,7 +114,7 @@ namespace BcfToolkit.Model.Bcf21
         [System.ComponentModel.DescriptionAttribute("It is currently limited to a value between 45 and 60 degrees. This limitation wil" +
             "l be dropped in the next release and viewers should be expect values outside thi" +
             "s range in current implementations.")]
-        [System.ComponentModel.DataAnnotations.RangeAttribute(typeof(double), "45", "60")]
+        // [System.ComponentModel.DataAnnotations.RangeAttribute(typeof(double), "45", "60")]
         [System.ComponentModel.DataAnnotations.RequiredAttribute(AllowEmptyStrings=true)]
         [System.Xml.Serialization.XmlElementAttribute("FieldOfView")]
         public double FieldOfView { get; set; }
